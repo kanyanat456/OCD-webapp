063ac2450063ce4abcf795cf233163ec7a6c50dc
“natthaporn6540301816” <natthaporn.we@ku.th>
2025-03-13 22:33:41 +0700

    first commit

---
+++ src/app/(routeapp)/contact/page.tsx
@@ -0,0 +1,11 @@
+import Contact from '@/app/components/Contact'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <Contact/>
+    </div>
+  )
+}
+
---
+++ src/app/(routeapp)/lifelonglearning/page.tsx
@@ -0,0 +1,10 @@
+import Lifelonglearning from '@/app/components/Lifelonglearning'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <Lifelonglearning/>
+    </div>
+  )
+}
